fad8c05e3fd77fc9c7e33596f8d66b822799a00a
RupinderVirk <56789961+RupinderVirk@users.noreply.github.com>
2019-10-21 13:47:26 +1000

    Add files via upload

---
+++ cms/css/news.php
@@ -0,0 +1,219 @@
+<?php 
+  session_start();
+ if(!isset($_SESSION['user']))
+ {
+   header('location:index.php');
+  
+ }
+  else
+  {
+  
+      if( isset ($_GET['id']))
+	  {
+		$id = $_GET['id'];
+	  }
+	  else
+	  {
+		$id = 0;
+	  }
+  
+     include '../error/manage.php';
+	 $sql = "select * from news order by year, month, date ASC";
+	 $result = mysqli_query($con, $sql);
+	
+   
+   ?>
+<?php include '_head.php' ?>
+
+<style>
+    #news
+	{
+	 
+      background: linear-gradient(60deg, #B24592, #F15F79);
+color:#fff;
+	}
+</style>
+
+  <body>
+
+    <?php include 'header.php' ?>
+
+    <div class="container-fluid">
+      <div class="row">
+        <?php include 'sidebar.php' ?>
+        <div class="col-sm-9  col-md-10  main">
+		<?php 
+		    if($id == 1)
+			{
+			   ?>
+			   <div class="alert alert-success alert-dismissable">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                       Event Successfully Added.
+               </div>
+							
+		<?php } ?>
+		
+		
+		<?php 
+		    if($id == 2)
+			{
+			   ?>
+			   <div class="alert alert-success alert-dismissable">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                       Event Updated.
+               </div>
+							
+		<?php } ?>
+		
+		<?php 
+		    if($id == 3)
+			{
+			   ?>
+			   <div class="alert alert-danger alert-dismissable">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                     Event Already Exists.
+               </div>
+							
+		<?php } ?>
+         
+          <h2 class="sub-header">All Events</h2><div class="newpage"><a href="newsadd.php">Add New Event</a></div>
+		  
+		  <div id="first-container">    
+          <div class="table-responsive">
+            <table class="table table-striped" id="rounded-corner">
+              <thead>
+			  
+			  
+                <tr>
+                 
+                  <th>Heading</th>
+				   <th>Date</th>
+                  <th>Edit </th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+			  
+			  
+                
+                 <?php
+			      while($row = mysqli_fetch_array($result))
+				  {
+				  date_default_timezone_set('Pacific/Auckland');
+				  
+				     $curyear = date('Y');
+					 $curmonth = date('m');
+					 $curdate = date('d');
+			   
+				   if($row['year'] > $curyear)
+				   {
+				   
+				  
+				?>
+              
+                <tr>
+                
+                
+                  <td><?php echo $row['heading']; ?></td>
+                  <td><?php echo $row['date']."-".$row['month']."-".$row['year']; ?></td>
+                  
+                  <td><a href="newsEdit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+                  <td><a href="delete.php?row=<?php echo $row['id']; ?>&tbl=news" class="ask"><img src="images/trash.png"></a></td>
+                </tr>
+				
+	  <?php } 
+	  
+	     elseif($row['year'] == $curyear)
+		 {
+		    if($row['month'] > $curmonth)
+             
+			 {
+			 ?>
+             
+              <tr>
+                
+                
+                  <td><?php echo $row['heading']; ?></td>
+                  <td><?php echo $row['date']."-".$row['month']."-".$row['year']; ?></td>
+                  
+                  <td><a href="newsEdit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+                  <td><a href="delete.php?row=<?php echo $row['id']; ?>&tbl=specification" class="ask"><img src="images/trash.png"></a></td>
+                </tr>
+             
+             <?php
+			 }		
+			 
+			 elseif($row['month'] == $curmonth)
+			 {
+			 
+			    if($row['date'] >= $curdate)
+				{
+				
+				?>
+                
+                <tr>
+                
+                
+                  <td><?php echo $row['heading']; ?></td>
+                  <td><?php echo $row['date']."-".$row['month']."-".$row['year']; ?></td>
+                  
+                  <td><a href="newsEdit.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+                  <td><a href="delete.php?row=<?php echo $row['id']; ?>&tbl=specification" class="ask"><img src="images/trash.png"></a></td>
+                </tr>
+                
+                <?php
+				
+				}
+			 
+			 } 
+		  
+		 }
+	    }
+		
+		
+	  ?>	
+               
+               
+              </tbody>
+            </table>
+          </div>
+        
+		<div class="my-navigation">
+			<div class="simple-pagination-first page-btn"></div>
+			<div class="simple-pagination-previous page-btn"></div>
+			<div class="simple-pagination-page-numbers page-btn"></div>
+			<div class="simple-pagination-next page-btn"></div>
+			<div class="simple-pagination-last page-btn"></div>
+	</div>
+	<div class="simple-pagination-page-x-of-x"></div>
+	<div class="simple-pagination-showing-x-of-x"></div>
+
+
+<div class="page-display">
+		Display <select class="simple-pagination-items-per-page"></select> items per page.
+	</div >
+	<div class="page-display">
+		Go directly to page <select class="simple-pagination-select-specific-page"></select>.
+	</div>
+</div>
+ <script src="js/jquery-simple-pagination-plugin.js"></script>
+
+<script>
+(function($){
+
+$('#first-container').simplePagination({
+     items_per_page: 20
+});
+
+})(jQuery);
+</script>
+	
+</div>	
+		
+      </div>
+    </div>
+	
+	<?php include 'footer.php' ?>
+  </body>
+</html>
+<?php } ?>
